2079df63dae182396b5db3960f799f496543ad88
sota_masuda <sota.m777121@gmail.com>
2021-02-09 01:20:36 +0900

    add operation db

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"strconv"
+
     "github.com/gin-gonic/gin"
     "github.com/jinzhu/gorm"
-
     _ "github.com/mattn/go-sqlite3" // DB操作はGORMでやるので使わないという意味で - をつける
 )
 
@@ -95,11 +96,69 @@ func main() {
     router := gin.Default()
     router.LoadHTMLGlob("templates/*.html")
 
-	data := "This string is set in main.go"
+	dbInit()
 
+	// Index
     router.GET("/", func(ctx *gin.Context){
-        ctx.HTML(200, "index.html", gin.H{"data": data})
+        todos := dbGetAll()
+		ctx.HTML(200, "index.html", gin.H{
+			"todos": todos,
+		})
     })
+
+	// Create with form
+	router.POST("/new", func(ctx *gin.Context) {
+		text := ctx.PostForm("text")
+		status := ctx.PostForm("status")
+		dbInsert(text, status)
+		ctx.Redirect(302, "/")
+	})
+
+	// Detail
+	router.GET("/detail/:id", func(ctx *gin.Context) {
+		n := ctx.Param("id")
+		id, err := strconv.Atoi(n)
+		if err != nil {
+			panic(err)
+		}
+		todo := dbGetOne(id)
+		ctx.HTML(200, "detail.html", gin.H{"todo": todo})
+	})
+
+	// Update
+	router.POST("/update/:id", func(ctx *gin.Context) {
+		n := ctx.Param("id")
+		id, err := strconv.Atoi(n)
+		if err != nil {
+			panic("ERROR")
+		}
+		text := ctx.PostForm("text")
+		status := ctx.PostForm("status")
+		dbUpdate(id, text, status)
+		ctx.Redirect(302, "/")
+	})
+
+	// Check item deleted
+	router.GET("/delete_check/:id", func(ctx *gin.Context){
+		n := ctx.Param("id")
+		id, err := strconv.Atoi(n)
+		if err != nil {
+			panic("ERROR")
+		}
+		todo := dbGetOne(id)
+		ctx.HTML(200, "delete.html", gin.H{"todos": todo})
+	})
+
+	// Delete
+	router.POST("/delete/:id", func(ctx *gin.Context){
+		n := ctx.Param("id")
+		id, err := strconv.Atoi(n)
+		if err != nil {
+			panic("ERROR")
+		}
+		dbDelete(id)
+		ctx.Redirect(302, "/")
+	})
 
     router.Run()
 }
