b8b8360967b85e29043be16229592fd2a8a8a430
sota_masuda <sota.m777121@gmail.com>
2021-02-09 00:41:13 +0900

    hello world in html

---
+++ go.mod
@@ -0,0 +1,5 @@
+module example.com/todo-app
+
+go 1.16
+
+require github.com/gin-gonic/gin v1.6.3 // indirect
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "github.com/gin-gonic/gin"
+)
+
+func main() {
+    router := gin.Default()
+    router.LoadHTMLGlob("templates/*.html")
+
+	data := "This string is set in main.go"
+
+    router.GET("/", func(ctx *gin.Context){
+        ctx.HTML(200, "index.html", gin.H{"data": data})
+    })
+
+    router.Run()
+}
